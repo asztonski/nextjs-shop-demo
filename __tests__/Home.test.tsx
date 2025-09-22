3e5d652214f9e18a6fc15aa85f7fe4a7b5d411d8
Dominik Supinski <dominik.supinski@enjoytravel.com>
2025-09-21 19:22:46 -0500

    Add the first Home page test

---
+++ __tests__/Home.test.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Home from "../app/page";
+
+describe("Home page", () => {
+  it("renders a heading", () => {
+    render(<Home />);
+    expect(
+      screen.getByRole("heading", { name: /get started/i })
+    ).toBeInTheDocument();
+  });
+});
