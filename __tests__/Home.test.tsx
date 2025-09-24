9c28a0779923a69adcc4c2f6385bf7070b377639
asztonski <asztonski@gmail.com>
2025-09-24 11:18:20 -0500

    Update assertion to match current header navigation

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+// __tests__/Home.test.tsx
 import { render, screen } from "@testing-library/react";
 import Home from "../app/page";
 
 describe("Home page", () => {
-  it("renders a heading", () => {
+  it("renders header navigation", () => {
     render(<Home />);
     expect(
-      screen.getByRole("link", { name: /read our docs/i })
+      screen.getByRole("link", { name: /next shop/i })
+    ).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", { name: /marketplace/i })
+    ).toBeInTheDocument();
+    expect(screen.getByRole("link", { name: /rankings/i })).toBeInTheDocument();
+    expect(
+      screen.getByRole("link", { name: /connect a wallet/i })
     ).toBeInTheDocument();
   });
 });
