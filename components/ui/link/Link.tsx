e2b247276d629b24df057922536a411da4bc1977
asztonski <asztonski@gmail.com>
2025-09-24 10:15:50 -0500

    Add ButtonLink component variety

--- components/ui/link/Link.tsx
+++ components/ui/link/Link.tsx
@@ -15,3 +15,22 @@ export const NavLink = ({
     </Link>
   );
 };
+
+export const ButtonLink = ({
+  children,
+  href,
+  className,
+}: Readonly<{
+  children: React.ReactNode;
+  href: string;
+  className?: string;
+}>) => {
+  return (
+    <Link
+      href={href}
+      className={`py-4 px-8 rounded-[20px] bg-accent flex items-center gap-4 font-semibold capitalize tap ${className}`}
+    >
+      {children}
+    </Link>
+  );
+};
