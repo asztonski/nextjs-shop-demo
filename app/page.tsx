3bb0aa2c9e2386b75172c4fe916b9786ea30b562
asztonski <asztonski@gmail.com>
2025-09-25 13:53:56 -0500

    Add simple main and footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import { Header } from "@/components/ui/header/Header";
+import { Footer } from "@/components/ui/footer/Footer";
 
 export default function Home() {
   return (
-    <div className="">
+    <div className="flex flex-col min-h-screen">
       <Header />
+      <main className="flex-grow"></main>
+      <Footer />
     </div>
   );
 }
