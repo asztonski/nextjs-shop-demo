bbcc297a273a707bdc026d27fc516d5d49f41754
asztonski <asztonski@gmail.com>
2025-09-25 14:03:51 -0500

    Add basic DOM structure

---
+++ components/ui/footer/Footer.tsx
@@ -0,0 +1,10 @@
+export const Footer = () => {
+  return (
+    <footer className="w-full bg-background-muted py-8 px-4">
+      <div className="container flex flex-col mx-auto p-3">
+        <div className="w-full flex justify-between items-center"></div>
+        <div className="w-full border-t-2"></div>
+      </div>
+    </footer>
+  );
+};
