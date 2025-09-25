725b7419cd2950c437116622241c2cc483145731
Dominik Supiński <asztonski@gmail.com>
2025-09-25 16:11:56 -0500

    Update font families (#9)
    * Introduce new color variable
    
    * Add color-celeste variable to theme
    
    * Add celeste-dark color variable to theme
    
    * Add text-box-title variable to theme
    
    * Refactor font variables in globals.css and layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,17 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Work_Sans, Space_Mono } from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const workSans = Work_Sans({
+  variable: "--font-work-sans",
   subsets: ["latin"],
+  weight: ["400", "600", "700"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const spaceMono = Space_Mono({
+  variable: "--font-space-mono",
   subsets: ["latin"],
+  weight: ["400", "700"],
 });
 
 export const metadata: Metadata = {
@@ -25,7 +27,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${workSans.variable} ${spaceMono.variable} antialiased`}
       >
         {children}
       </body>
