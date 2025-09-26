6502dffaa016eea297abb052040c6607d12c4154
asztonski <asztonski@gmail.com>
2025-09-26 16:43:40 -0500

    Refactor Home.test.tsx to test header, main, and footer landmarks

--- __tests__/Home.test.tsx
+++ __tests__/Home.test.tsx
@@ -1,19 +1,23 @@
 // __tests__/Home.test.tsx
 import { render, screen } from "@testing-library/react";
 import Home from "../app/page";
+import RootLayout from "../app/layout";
 
 describe("Home page", () => {
-  it("renders header navigation", () => {
-    render(<Home />);
-    expect(
-      screen.getByRole("link", { name: /next shop/i })
-    ).toBeInTheDocument();
-    expect(
-      screen.getByRole("link", { name: /marketplace/i })
-    ).toBeInTheDocument();
-    expect(screen.getByRole("link", { name: /rankings/i })).toBeInTheDocument();
-    expect(
-      screen.getByRole("link", { name: /connect a wallet/i })
-    ).toBeInTheDocument();
+  it("renders header, main and footer landmarks", () => {
+    render(
+      <RootLayout>
+        <Home />
+      </RootLayout>
+    );
+
+    // <header> => role="banner"
+    expect(screen.getByRole("banner")).toBeInTheDocument();
+
+    // <main> => role="main"
+    expect(screen.getByRole("main")).toBeInTheDocument();
+
+    // <footer> => role="contentinfo"
+    expect(screen.getByRole("contentinfo")).toBeInTheDocument();
   });
 });
