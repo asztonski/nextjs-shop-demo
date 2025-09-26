43e81e2707219d11f4ff868e2199395583520e35
asztonski <asztonski@gmail.com>
2025-09-26 15:59:20 -0500

    Add icon prop to Button component for optional icon rendering. Improve styling rules

--- components/ui/button/Button.tsx
+++ components/ui/button/Button.tsx
@@ -1,17 +1,22 @@
+import Image from "next/image";
+
 export const Button = ({
   children,
   onClick,
   className,
+  icon,
 }: Readonly<{
   children: React.ReactNode;
   onClick?: () => void;
   className?: string;
+  icon?: string;
 }>) => {
   return (
     <button
       onClick={onClick}
-      className={`py-4 px-8 rounded-[20px] bg-accent flex items-center gap-4 font-semibold capitalize tap ${className}`}
+      className={`px-8 rounded-[20px] bg-accent flex items-center gap-3 font-semibold capitalize tap ${className}`}
     >
+      {icon && <Image src={icon} width={20} height={20} alt={`${icon} icon`} />}
       {children}
     </button>
   );
