b8aa109aa4d2784cf70cf1550c011f05653d55b0
asztonski <asztonski@gmail.com>
2025-09-26 16:46:44 -0500

    Refactor postcss.config.mjs to remove autoprefixer and streamline plugin configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,5 @@
-// postcss.config.mjs
-import tailwindPlugin from "@tailwindcss/postcss";
-import autoprefixer from "autoprefixer";
-
-export default {
-  plugins: [tailwindPlugin(), autoprefixer()],
+const config = {
+  plugins: ["@tailwindcss/postcss"],
 };
+
+export default config;
