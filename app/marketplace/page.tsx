111b0bdf90004fc583100c46f9958ce507778a9c
asztonski <asztonski@gmail.com>
2025-10-27 18:07:41 -0500

    Add About component for Marketplace page

---
+++ app/marketplace/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="flex">
+      <h1 className="text-4xl font-bold text-center">Marketplace</h1>
+    </div>
+  );
+}
