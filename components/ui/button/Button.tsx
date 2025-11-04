55f7ad3c4e021a9be5833176a609f09a7ad1ec1d
asztonski <asztonski@gmail.com>
2025-11-03 20:07:07 -0500

    Add type prop to Button component for better button type handling

--- components/ui/button/Button.tsx
+++ components/ui/button/Button.tsx
@@ -5,16 +5,19 @@ export const Button = ({
   onClick,
   className,
   icon,
+  type,
 }: Readonly<{
   children: React.ReactNode;
   onClick?: () => void;
   className?: string;
   icon?: string;
+  type?: "button" | "submit" | "reset";
 }>) => {
   return (
     <button
       onClick={onClick}
       className={`px-8 rounded-[20px] w-max bg-accent flex items-center justify-center gap-3 font-semibold capitalize tap ${className}`}
+      type={type}
     >
       {icon && <Image src={icon} width={20} height={20} alt={`${icon} icon`} />}
       {children}
