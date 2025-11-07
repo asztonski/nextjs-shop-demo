4a0ff35830c27a428f3d65ac0effe1944505b7fd
asztonski <asztonski@gmail.com>
2025-11-06 20:01:01 -0500

    Enhance TextField component to support validation with error messaging

--- components/ui/input/TextField.tsx
+++ components/ui/input/TextField.tsx
@@ -1,3 +1,6 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Image from "next/image";
 
 export const TextField = ({
@@ -8,6 +11,10 @@ export const TextField = ({
   inputClassName,
   onChange,
   type,
+  isInvalid,
+  errorMessage,
+  onBlur,
+  name,
 }: {
   placeholder: string;
   value: string;
@@ -16,29 +23,58 @@ export const TextField = ({
   inputClassName?: string;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
   type?: string;
+  isInvalid?: boolean;
+  errorMessage?: string;
+  onBlur?: (e: React.FocusEvent<HTMLInputElement>) => void;
+  name?: string;
 }) => {
+  const [errorVisible, setErrorVisible] = useState(false);
+
+  useEffect(() => {
+    if (isInvalid) {
+      setErrorVisible(isInvalid);
+    } else {
+      setErrorVisible(false);
+    }
+  }, [isInvalid]);
+
   return (
-    <div
-      className={`rounded-full bg-foreground relative text-background w-full p-2 overflow-hidden ${className}`}
-    >
-      <div className="lg:absolute bg-inherit left-0 top-0 h-full w-full flex items-center gap-3 pl-11">
-        {icon && (
-          <Image
-            src={icon}
-            width={20}
-            height={20}
-            alt={`${icon} icon`}
-            className="absolute left-5 top-1/2 transform -translate-y-1/2"
+    <div className="relative">
+      <div
+        className={`rounded-full bg-foreground relative text-background w-full p-2 overflow-hidden ${
+          isInvalid ? "border-2 border-red-500" : ""
+        } ${className}`}
+      >
+        <div className="lg:absolute bg-inherit left-0 top-0 h-full w-full flex items-center gap-3 pl-11">
+          {icon && (
+            <Image
+              src={icon}
+              width={20}
+              height={20}
+              alt={`${icon} icon`}
+              className="absolute left-5 top-1/2 transform -translate-y-1/2"
+            />
+          )}
+          <input
+            type={type || "text"}
+            placeholder={placeholder}
+            value={value}
+            onChange={onChange}
+            className={`p-2 bg-inherit ${inputClassName}`}
+            onBlur={onBlur}
+            name={name}
           />
-        )}
-        <input
-          type={type || "text"}
-          placeholder={placeholder}
-          value={value}
-          onChange={onChange}
-          className={`p-2 bg-inherit ${inputClassName}`}
-        />
+        </div>
       </div>
+      {isInvalid && (
+        <div
+          className={`w-72 bg-foreground absolute left-0 right-0 m-auto -bottom-17 z-10 transition-opacity duration-200 ease-in-out border-red-500 border p-2 rounded ${
+            errorVisible ? "opacity-100" : "opacity-0"
+          }`}
+        >
+          <p className="text-sm text-black">{errorMessage}</p>
+        </div>
+      )}
     </div>
   );
 };
