fddd2a616ad252963496ec3acaf35c9777fce5b7
asztonski <asztonski@gmail.com>
2025-11-08 15:45:11 -0500

    Implement sign-in page layout with image and form components

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,7 +1,31 @@
 import React from "react";
+import Image from "next/image";
+import HeroImage from "@/public/sign-up-hero.jpg";
+import { SignInForm } from "@/components/ui/form/SignInForm";
 
 export default function SignUpPage() {
   return (
-    <div className="flex flex-col lg:flex-row gap-4 lg:gap-15 lg:x-auto w-full lg:w-auto py-6 lg:py-0"></div>
+    <div className="flex flex-col lg:flex-row gap-4 lg:gap-15 lg:x-auto w-full lg:w-auto py-6 lg:py-0">
+      <div className="w-full lg:flex-1 relative h-96 lg:h-auto">
+        <Image
+          src={HeroImage}
+          alt="Sign Up"
+          fill
+          className="object-cover"
+          priority
+          // sizes="(max-width: 768px) 200vw, 40vw"
+        />
+      </div>
+      <div className="lg:flex-1 max-w-sm mx-auto lg:max-w-none px-4 lg:px-0">
+        <div className="py-4 lg:py-24 lg:max-w-md w-full mx-auto lg:mx-0">
+          <h1 className="text-4xl lg:text-5xl font-semibold">Sign in</h1>
+          <h2 className="text-base lg:text-box-subtitle mt-6 capitalize">
+            Log in to your account and Start Creating, Collecting And Selling
+            Nfts.
+          </h2>
+          <SignInForm />
+        </div>
+      </div>
+    </div>
   );
 }
