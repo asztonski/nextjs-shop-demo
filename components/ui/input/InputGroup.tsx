409688320d0704ea8fe550f8055806605a9b2adc
asztonski <asztonski@gmail.com>
2025-11-11 18:44:49 -0500

    Refactor InputGroup component for improved styling and layout consistency

--- components/ui/input/InputGroup.tsx
+++ components/ui/input/InputGroup.tsx
@@ -17,9 +17,9 @@ export const InputGroup = ({
   textFieldPlaceholder,
 }: InputGroupProps) => {
   return (
-    <div className="flex flex-col  gap-4 lg:gap-0 lg:flex-row w-full sm:w-3/4 lg:w-105 relative lg:h-15 overflow-hidden">
+    <div className="flex flex-col gap-4 lg:gap-0 lg:flex-row w-full sm:w-3/4 lg:w-105 lg:h-15 relative overflow-hidden">
       <TextField
-        className="w-full h-full !border-none !rounded-[20px]"
+        className="w-full h-full !border-none !rounded-[20px] !p-0"
         placeholder={textFieldPlaceholder}
         value={textFieldValue}
         onChange={(e) => {
@@ -27,7 +27,7 @@ export const InputGroup = ({
         }}
       />
       <Button
-        className="static lg:absolute text-foreground !w-44 right-0 top-0 lg:h-full"
+        className="static lg:absolute text-foreground right-0 top-0 !py-2 !w-full lg:!w-42 lg:!h-full"
         icon={buttonIcon}
       >
         {buttonText}
